3174b46acc914737995c2ca70ce940f4ce6f7df3
Dustin Tinney <tinney@gmail.com>
2020-01-19 12:56:00 -0800

    Updating game script

--- bin/play_game.rb
+++ bin/play_game.rb
@@ -13,43 +13,32 @@ TEAM_ID = "22"
 BASE_URL = "http://localhost:3000/api"
 HEADERS = {'Content-Type' =>'application/json', 'TEAM' => TEAM_ID }
 
-def create_player(name:, water_stat:, food_stat:, stamina_stat:, strength_stat:)
-  uri = URI.parse("#{BASE_URL}/players/")
+def make_request(api_endpoint, request_body)
+  uri = URI.parse("#{BASE_URL}/#{api_endpoint}/")
   http = Net::HTTP.new(uri.host, uri.port)
 
   req = Net::HTTP::Post.new(uri.path, HEADERS)
-  req.body = {name: name, water_stat: water_stat, food_stat: food_stat, stamina_stat: stamina_stat, strength_stat: strength_stat}.to_json
+  req.body = request_body.to_json
   res = http.request(req)
 
-  puts "response #{res.body}"
-  puts JSON.parse(res.body)
+  return JSON.parse(res.body)
+  
   rescue => e
-      puts "failed #{e}"
+    puts "failed #{e}"
 end
 
 puts "Playing the game"
 puts "Creating a player"
-create_player(name: "Player w/ EQ Stats", water_stat: 5, food_stat: 5, stamina_stat: 5, strength_stat: 5)
+response = make_request("players", {name: "Player w/ EQ Stats", water_stat: 5, food_stat: 5, stamina_stat: 5, strength_stat: 5})
 
+puts "Moving the player around"
 
-# post to get a new player...  with food_size, water_size, stamina, ...
-# res = Net::HTTP.post_form(uri, direction: 'SOUTH')
-# puts res.body
+active = true
 
-# post to create a player for your team
-# post /players/:id/moves, direction: 'NORTH SOUTH EAST WEST'
-
-# request_body = { direction: "SOUTH" }
-
-# # Create the HTTP objects
-# http = Net::HTTP.new(uri.host, uri.port)
-# request = Net::HTTP::Post.new(uri.request_uri, header)
-# request.body = request_body.to_json
-# response = http.request(request)
-
-
-# 300.times do
-#   res = Net::HTTP.post_form(uri, direction: 'SOUTH')
-# end
-# puts res.body
-
+while(active)
+  direction = ["NORTH", "SOUTH", "EAST", "WEST"].sample(1).first
+  response = make_request("moves", { direction: "North" })
+  puts response
+  actve = response["player"]["active"]
+  sleep(0.5)
+end
